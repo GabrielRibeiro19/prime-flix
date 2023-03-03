749699863e384ee092e6606742f770fa95b65b37
TBRWEB - Desenvolvimento de Sites <105514730+GabrielRibeiro19@users.noreply.github.com>
2023-03-03 17:52:33 -0300

    update filmes

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import api from '../../services/api';
+
+
  function Filme(){
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function loadFilme(){
+      await api.get(`/movie/${id}`, {
+        params:{
+          api_key: "19c9e0f6f411f09779144ba99aac702e",
+          language: "pt-BR",
+        }
+      })
+    }
+    loadFilme();
+  }, [])
+
   return(
     <div>
-      <h1>BEM VINDO A PÁGINA DETALHES DO FILME</h1>
+      <h1>ACESSANDO O FILME: {id}</h1>
     </div>
   )
  }
